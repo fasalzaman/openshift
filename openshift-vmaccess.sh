500aa10813a27bdb2241a8cfcfbd869ea96f311e
fasalzaman <fasal.zaman@gmail.com>
2018-08-06 14:23:20 +0530

    Update openshift-vmaccess.sh

--- openshift-vmaccess.sh
+++ openshift-vmaccess.sh
@@ -1,5 +1,11 @@
 
 echo "********************************************************************************************"
+	echo "`date` -- Adding student to wheel group for sudo access'" >>/root/lsprovision.log
+	usermod -G wheel demouser
+echo "********************************************************************************************"
+	echo "`date` -- Setting Root Password to 'Microsoft'" >>/root/lsprovision.log
+	echo "demoPassword1!" | passwd --stdin root
+echo "********************************************************************************************"
 	echo "`date` -- Creating required logical volumes" >>/root/lsprovision.log
 	lvcreate -n libvirtlv -L+20G rootvg
 	mkfs -t ext4 /dev/rootvg/libvirtlv
